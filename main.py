12af88f9e23bfc7dcbbaad45691aa5187669c10a
Derek Pecka <derek@pecka.io>
2024-07-22 08:33:24 -0700

    fix bug that prevents schedules from toggling

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import time
 import signal
 import schedule
+import datetime
 
 from util import log
 import equipment
@@ -9,7 +10,12 @@ import configuration
 
 
 letusgrow = None
+
 sigint_count = 0
+
+
+schedule_rotator = schedule.Scheduler()
+schedule_runner = schedule.Scheduler()
 
 
 # if the app is forced to quit, try gracefully shutting down the tower
@@ -30,23 +36,26 @@ signal.signal(signal.SIGINT, signal_handler)
 def daytime_schedule(letusgrow_tower: equipment.LetUsGrowTower):
     log("Event=StartingSchedule, "
         "Schedule=Daytime")
-    schedule.clear()
-    letusgrow_tower.evaluate_chemistry()
-    schedule.every(10).minutes.do(lambda tower: tower.lights.on(), letusgrow_tower)
-    schedule.every(1).hours.do(lambda tower: tower.evaluate_chemistry(), letusgrow_tower)
-    schedule.every(15).minutes.do(lambda tower: tower.watering_pump.toggle(), letusgrow_tower)
-    schedule.every(10).minutes.do(lambda tower: tower.state_audit(), letusgrow_tower)
+
+    schedule_runner.clear()
+    letusgrow_tower.lights.on()
+
+    schedule_runner.every(1).hours.do(lambda tower: tower.evaluate_chemistry(), letusgrow_tower)
+    schedule_runner.every(15).minutes.do(lambda tower: tower.watering_pump.toggle(), letusgrow_tower)
+    schedule_runner.every(30).minutes.do(lambda tower: tower.state_audit(), letusgrow_tower)
 
 
 def nighttime_schedule(letusgrow_tower: equipment.LetUsGrowTower):
     log("Event=StartingSchedule, "
         "Schedule=Nighttime")
-    schedule.clear()
-    # letusgrow_tower.lights.off()
-    schedule.every(10).minutes.do(lambda tower: tower.lights.off(), letusgrow_tower)
-    schedule.every().hour.at(":00").do(lambda tower: tower.watering_pump.on(), letusgrow_tower)
-    schedule.every().hour.at(":15").do(lambda tower: tower.watering_pump.off(), letusgrow_tower)
-    schedule.every(10).minutes.do(lambda tower: tower.state_audit(), letusgrow_tower)
+
+    schedule_runner.clear()
+    letusgrow_tower.lights.off()
+
+    schedule_runner.every(1).hours.do(lambda tower: tower.evaluate_chemistry(), letusgrow_tower)
+    schedule_runner.every().hour.at(":00").do(lambda tower: tower.watering_pump.on(), letusgrow_tower)
+    schedule_runner.every().hour.at(":15").do(lambda tower: tower.watering_pump.off(), letusgrow_tower)
+    schedule_runner.every(30).minutes.do(lambda tower: tower.state_audit(), letusgrow_tower)
 
 
 if __name__ == '__main__':
@@ -54,11 +63,17 @@ if __name__ == '__main__':
     letusgrow = equipment.LetUsGrowTower(relay_board)
 
     # default to initially running the daytime schedule once
-    daytime_schedule(letusgrow)
+    daytime_start_time = datetime.datetime.strptime(configuration.DAYTIME_SCHEDULE_START_TIME, "%H:%M")
+    nighttime_start_time = datetime.datetime.strptime(configuration.NIGHTTIME_SCHEDULE_START_TIME, "%H:%M")
+    if daytime_start_time <= datetime.datetime.now() < nighttime_start_time:
+        daytime_schedule(letusgrow)
+    else:
+        nighttime_schedule(letusgrow)
 
-    schedule.every().day.at(configuration.DAYTIME_SCHEDULE_START_TIME).do(daytime_schedule, letusgrow)
-    schedule.every().day.at(configuration.NIGHTTIME_SCHEDULE_START_TIME).do(nighttime_schedule, letusgrow)
+    schedule_rotator.every().day.at(configuration.DAYTIME_SCHEDULE_START_TIME).do(daytime_schedule, letusgrow)
+    schedule_rotator.every().day.at(configuration.NIGHTTIME_SCHEDULE_START_TIME).do(nighttime_schedule, letusgrow)
     
     while True:
-        schedule.run_pending()
-        time.sleep(1)
+        schedule_rotator.run_pending()
+        schedule_runner.run_pending()
+        time.sleep(5)
